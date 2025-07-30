c2507174e20bd0d8d5b44f70c9025eb435f9e0b9
John Hartley <41830007+j-hartley@users.noreply.github.com>
2025-07-30 16:25:22 +0100

    Simplify stream pipeline

--- src/cmd/pack.ts
+++ src/cmd/pack.ts
@@ -59,9 +59,6 @@ export default async function pack(
     filePaths,
     targetCarSize
   )) {
-    let rootCID = placeholderCID;
-    let pieceCID = placeholderCID;
-
     const subManifest = manifest.newSubManifest();
     // We use a UUID for a temporary piece file name, we will renamr it later
     // after the root (payload) CID is known.
@@ -74,27 +71,13 @@ export default async function pack(
       subManifest
     );
 
-    const carStream = stream
-      .pipeThrough(
-        new TransformStream<Block, Block>({
-          transform(block, controller) {
-            // we visit every block, the last one will be the root block
-            rootCID = CID.parse(block.cid.toString());
-
-            // console.log("Root CID:", rootCID.toString());
-            controller.enqueue(block);
-          },
-        })
-      )
-      .pipeThrough(new CAREncoderStream([placeholderCID]));
-
-    // Annoyingly we now have to stream the whole lot again as Piece Commitment
-    // requires FR32 padding the finished CAR file, so fork it and process in parallel.
-    const [toFile, toCommP] = carStream.tee();
     const { stream: commPTransform, getCommP } = createCommPStream();
-    await Promise.all([
-      // One stream goes to disk...
-      toFile.pipeTo(
+    const carEncoder = new CAREncoderStream([placeholderCID])
+
+    await stream
+      .pipeThrough(carEncoder)
+      .pipeThrough(commPTransform)
+      .pipeTo(
         Writable.toWeb(
           createWriteStream(join(opts.output, pieceTemporaryFilename), {
             autoClose: true,
@@ -103,24 +86,19 @@ export default async function pack(
             highWaterMark: 1,
           })
         ) as WritableStream<Buffer>
-      ),
+      );
 
-      // ...the other stream goes to the commP transform.
-      toCommP.pipeThrough(commPTransform).pipeTo(
-        new WritableStream({
-          write() {
-            /* noop, just ensure we're drained before fetching commP */
-          },
-        })
-      ),
-    ]);
+    if (!carEncoder.finalBlock) {
+      throw new Error("Failed to get final block from CAR stream")
+    }
+    const rootCID = CID.parse(carEncoder.finalBlock.cid.toString())
 
     // Bit tortured to get the CID as FilOz use a 'legacy' type in preparation for Piece V2.
     const streamCommP = getCommP();
     if (!streamCommP) {
       throw new Error("Failed to get CommP from stream");
     }
-    pieceCID = CID.parse(streamCommP.toString());
+    const pieceCID = CID.parse(streamCommP.toString());
 
     console.log(
       `Packing completed, root CID: ${rootCID.toString()}, piece CID: ${pieceCID.toString()}`

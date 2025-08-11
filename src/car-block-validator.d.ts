f980dd41caa59c15d62f7fde2e8604f1c7c33d04
John Hartley <41830007+j-hartley@users.noreply.github.com>
2025-08-11 15:16:45 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add unpack support

--- src/car-block-validator.d.ts
+++ src/car-block-validator.d.ts
@@ -1,6 +1,5 @@
 // The original package does not have a type definition file, so we create a minimal one here.
 
-declare module '@web3-storage/car-block-validator' {
+declare module "@web3-storage/car-block-validator" {
   export function validateBlock(block: Block): Promise<void>;
 }
-
